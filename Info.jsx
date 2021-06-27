69b2d3c0475ec63fdcba775ccf0b10a3f8b6612f
itschinmay19 <83499095+itschinmay19@users.noreply.github.com>
2021-06-27 11:48:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>
+        Gaming and Technology</h1>
+      <p>
+        YourGuider Chinmay brings you the indepth reviews about the tech products as
+        well as the latest news about the gaming
+      </p>
+    </div>
+  
+  );
+}
+export default Info;
